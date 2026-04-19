da8a058b255e23a5aafd37d1c689d9f7a9f5ad71
aebonlee <aebon@kyonggi.ac.kr>
2026-04-19 11:14:12 +0900

    Updates

--- assets/CPractice-BYJLyvq5.js
+++ assets/CPractice-BYJLyvq5.js
@@ -1,4 +1,4 @@
-import{u as m,r as f,j as n}from"./index-o2ePIImh.js";import{C as h}from"./CodeEditor-DDLSR1DA.js";const x=[{step:1,titleKo:"기초 출력",titleEn:"Basic Output",descKo:"printf를 사용하여 다양한 출력을 연습합니다.",descEn:"Practice various outputs using printf.",examples:[{titleKo:"Hello World 출력",titleEn:"Print Hello World",code:`#include <stdio.h>
+import{u as m,r as f,j as n}from"./index-CPIg0ztY.js";import{C as h}from"./CodeEditor-APdGvzi4.js";const x=[{step:1,titleKo:"기초 출력",titleEn:"Basic Output",descKo:"printf를 사용하여 다양한 출력을 연습합니다.",descEn:"Practice various outputs using printf.",examples:[{titleKo:"Hello World 출력",titleEn:"Print Hello World",code:`#include <stdio.h>
 
 int main() {
     printf("Hello, World!\\n");
